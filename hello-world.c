b31f8b371901f97ebc91edebb93ee67ed236828a
David Feitosa <feitosadavid4240@hotmail.com>
2025-08-07 11:44:14 -0300

    Pastas e arquivo calc-rudimentar criados!

--- hello-world.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>	// Inclui a biblioteca padrão E/S
-#include <stdlib.h> // Inclui a biblioteca padrão de utilitários
-
-int main() {
-    printf("Ola mundo/n");
-
-    return 0; 				// Indica término com sucesso
-}
