978ae35e7a2002650dbf9142e1c42c439858ddd8
David Feitosa <feitosadavid4240@hotmail.com>
2025-08-07 11:35:52 -0300

    hello-world criado!

---
+++ hello-world.c
@@ -0,0 +1,8 @@
+#include <stdio.h>	// Inclui a biblioteca padrão E/S
+#include <stdlib.h> // Inclui a biblioteca padrão de utilitários
+
+int main() {
+    printf("Ola mundo/n");
+
+    return 0; 				// Indica término com sucesso
+}
